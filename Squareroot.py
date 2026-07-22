0dac20213c3c2fd4baa488003c5c4c878dd2318b
josh <jimmykim506@gmail.com>
2026-07-22 19:32:35 +1000

    Add files via upload

---
+++ Squareroot.py
@@ -0,0 +1,10 @@
+import math
+
+# Take a number as input from the user
+num = float(input("Enter a number: "))
+
+# Calculate the square root using math.sqrt()
+square_root = math.sqrt(num)
+
+# Display the result
+print("The square root is:", square_root)
