8c9bb4e0c6ee6c2012d9975f8fc4ee261288cb96
josh <jimmykim506@gmail.com>
2026-07-26 18:42:03 +1000

    Add files via upload

---
+++ numbergreatersmaller.py
@@ -0,0 +1,5 @@
+b=int(input("Enter A Number:"))
+if (b<15):
+    print("This B is smaller than 15")
+else:
+    print("Ths B is greater than 15")
---
+++ oddoreven.py
@@ -0,0 +1,5 @@
+b=int(input("Enter a number"))
+if b%2==0:
+    print("This is an even number")
+else:
+    print("This is an odd number")
