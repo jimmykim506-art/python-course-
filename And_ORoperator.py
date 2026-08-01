76c469a409f9346e521b4a6a3dbd8f03c633eb22
josh <jimmykim506@gmail.com>
2026-08-01 14:10:23 +1000

    Add files via upload

---
+++ And_ORoperator.py
@@ -0,0 +1,19 @@
+a=3
+b=20
+c=32
+if a and b and c:
+    print(" All the number have boolean value as true")
+else:
+    print("At least one number has boolean number as false")
+a=10
+b=-10
+c=0
+if a>0 or b>0:
+    print(" Either of the number is greater than 0")
+else:
+    print(" No number is greater than 0")
+
+if b>0 or c>0:
+    print(" Either of the number is greater than 0")
+else:
+      print(" No number is greater than 0")
