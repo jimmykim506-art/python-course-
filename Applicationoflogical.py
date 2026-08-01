76311419291c19264bd94f5f95251ccd7afe545a
josh <jimmykim506@gmail.com>
2026-08-01 14:10:57 +1000

    Add files via upload

---
+++ Applicationoflogical.py
@@ -0,0 +1,15 @@
+a=10
+b=12
+c=12
+print(not(a==b))
+print(not(b==c))
+
+
+
+a=4
+b=5
+if not ((a==1)) == ((b==5)):
+        print("Hello")
+a=int(input("Enter The Number"))
+if not (a%2==0):
+        print(a,"is an odd number")
