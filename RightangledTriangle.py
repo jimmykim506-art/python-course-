20fe2e9234c4ac544e251d992f786976c8cb9cd6
josh <jimmykim506@gmail.com>
2026-08-23 18:47:16 +1000

    Add files via upload

---
+++ RightangledTriangle.py
@@ -0,0 +1,21 @@
+
+print("Right Angle Triangle Pattern")
+
+
+n = int(input("Enter the number of rows: "))
+
+
+for i in range(n):
+    
+    for j in range(i + 1):
+        print("* ", end="")
+    
+    print()
+
+    print("Half Pyrmaid Pattern of Stars (*):")
+    n=int(input("Enter a number of rows: "))
+    for i in range(1,row +1)
+    for j in range (1, i+1):
+        print(number, end ='  ')
+        number=number+1
+        print()
