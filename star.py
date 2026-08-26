7594f76a7166a3ff06850ae3f957301c237dafc5
josh <jimmykim506@gmail.com>
2026-08-26 19:40:10 +1000

    Add files via upload

---
+++ star.py
@@ -0,0 +1,30 @@
+import turtle
+turtle.Screen().bgcolor("blue")
+turtle.Screen().setup(500,400)
+board=turtle.Turtle()
+board.forward(100)
+board.left(120)
+
+
+board.forward(100)
+board.left(120)
+board.forward(100)
+
+board.penup()
+board.right(150)
+board.forward(50)
+
+#second triangle
+board.pendown()
+board.right(90)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+board.right(120)
+board.forward(100)
+
+
+
+turtle.done()
